196308a1251b37ee6e407eeb07ca5afb297889db
miteshdalsaniya774 <miteshdalsaniya@jspinfotech.com>
2024-07-04 15:20:13 +0530

    product error

--- resources/views/products/product_list.blade.php
+++ resources/views/products/product_list.blade.php
@@ -11,18 +11,15 @@
                 <h2 class="card-title mb-0">Product Listing Page</h2>
             </div>
         </div>
-        <div class="d-flex justify-content-spacebetween">
+        <div class="d-flex justify-content-between">
             <form></form>
-            <div class=" ms-auto">
+            <div class="ms-auto">
                 <button onclick="window.location.href='{{ route('products.create') }}'"
                     class="btn btn-primary create-new waves-effect waves-light btn-dark" tabindex="0"
                     aria-controls="DataTables_Table_0" type="button"><span><i class="ti ti-plus me-sm-1"></i> Add
                         Product</span></button>
             </div>
         </div>
-
-        <!-- <a class="btn btn-success" href="{{ route('products.create') }}"> Add Product</a> -->
-
 
         @if ($message = Session::get('success'))
             <div class="alert alert-success">
@@ -36,26 +33,24 @@
                         <th class="sorting_disabled dt-checkboxes-cell dt-checkboxes-select-all" rowspan="1" colspan="1"
                             style="width: 18px;" data-col="1" aria-label=""><input type="checkbox"
                                 class="form-check-input round-checkbox"></th>
+                        <th>Product Image</th>
                         <th>Profile Info</th>
                         <th>Price</th>
                         <th>Qty</th>
-                        <!-- <th>Stock</th> -->
                         <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($products as $product)
                         <tr>
-                            <td class="  dt-checkboxes-cell"><input type="checkbox"
+                            <td class="dt-checkboxes-cell"><input type="checkbox"
                                     class="dt-checkboxes form-check-input round-checkbox">
                             </td>
+                            <td><img src="{{ asset('images/products/' . $product->product_image) }}" alt="{{ $product->product_name }}" width="100"></td>
                             <td>{{ $product->product_name }}</td>
-                            <!-- <td>{{ $product->product_code }}</td> -->
-                            <!-- <td>{{ $product->product_description }}</td> -->
                             <td>{{ $product->product_price }}</td>
                             <td>{{ $product->product_stock }}</td>
                             <td class="d-flex">
-
                                 <button type="button" class="btn px-1 py-0 edit-btn me-1 text-secondary btn-outline-light"
                                     onclick="window.location.href='{{ route('products.edit', $product->id) }}'">
                                     <i class="ti ti-pencil me-1"></i> Edit </button>
@@ -67,23 +62,11 @@
                                     @csrf
                                     @method('DELETE')
                                     <button type="button" class="btn p-2 delete-btn text-danger"
-                                        onclick="this.closest('form').submit();">
+                                        onclick="return confirmDelete();">
                                         <i class="ti ti-trash me-1"></i>
                                     </button>
                                 </form>
-
                             </td>
-                            
-                            <!-- <td>
-                                                                    <form action="{{ route('products.destroy', $product->id) }}" method="POST"
-                                                                        onsubmit="return confirmDelete()">
-                                                                        <a class="btn btn-info" href="{{ route('products.show', $product->id) }}">Show</a>
-                                                                        <a class="btn btn-primary" href="{{ route('products.edit', $product->id) }}">Edit</a>
-                                                                        @csrf
-                                                                        @method('DELETE')
-                                                                        <button type="submit" class="btn btn-danger">Delete</button>
-                                                                    </form>
-                                                                </td> -->
                         </tr>
                     @endforeach
                 </tbody>
@@ -95,11 +78,9 @@
 </div>
 
 <script>
-
     function confirmDelete() {
         return confirm('Are you sure you want to delete this product?');
     }
-
 </script>
 
 @endsection
