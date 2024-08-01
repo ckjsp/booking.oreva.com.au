898e77a6d676cd5ea3b6a84fef263e0fd0c62066
miteshdalsaniya774 <miteshdalsaniya@jspinfotech.com>
2024-08-01 18:06:25 +0530

    Update navbar.blade.php

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,5 +1,4 @@
 <nav class="navbar navbar-expand-lg navbar-light">
-
 <div class="container-fluid px-0">
 
   <p class="dashboard_text f-600 text-black"></p>
@@ -8,14 +7,7 @@
   </button>
   <div class="collapse navbar-collapse " id="navbarNav">
     <ul class="navbar-nav ms-auto align-items-center gap-2">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-menu-2">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M4 6l16 0" />
-        <path d="M4 12l16 0" />
-        <path d="M4 18l16 0" />
-      </svg>
-      <span class="text-black">MENU</span>
-      </li>
+   
       <li class="nav-item">
         <div class="avatar avatar-sm"><img src="{{ asset('img/Mask group (20).png') }}" alt="Avatar" class="rounded-circle pull-up" width="30px" height="30px"></div>
       </li>
