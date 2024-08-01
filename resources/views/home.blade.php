ef92a1d75e3544cadf25360ae60a6a5dec273eee
miteshdalsaniya774 <miteshdalsaniya@jspinfotech.com>
2024-08-01 17:58:38 +0530

    dashbored image issue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,41 +7,17 @@
   <!-- Including the sidebar partial -->
 
   <div class="content-wrapper pl-30">
-    <nav class="navbar navbar-expand-lg navbar-light">
-
-      <div class="container-fluid px-0">
-
-        <p class="dashboard_text f-600 text-black">Dashboard</p>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse " id="navbarNav">
-          <ul class="navbar-nav ms-auto align-items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-menu-2">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M4 6l16 0" />
-              <path d="M4 12l16 0" />
-              <path d="M4 18l16 0" />
-            </svg>
-            <span class="text-black">MENU</span>
-            </li>
-            <li class="nav-item">
-              <div class="avatar avatar-sm"><img src="{{ asset('img/Mask group (20).png') }}" alt="Avatar" class="rounded-circle pull-up" width="30px" height="30px"></div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  
     <div class="container px-0">
 
+    @include('include.navbar')
 
       <div class="row g-3">
         <!-- section 1 start -->
         <div class="col-12 col-sm-6  col-lg-3">
           <a href="{{ route('customers.index') }}" class="menu-link d-block">
 
-            <div class="dashboard-card card ">
-
+            <div class="dashboard-card card">
               <div class="d-flex justify-content-between mb-2">
                 <h5 class=" mb-0 fs-5 fw-bold">{{ $customerCount }}</h5>
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-users-group">
@@ -54,6 +30,7 @@
                   <path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
                 </svg>
               </div>
+
               <p class="text-secondary">Customers</p>
 
             </div>
@@ -147,7 +124,7 @@
               <div class="tab-pane fade show active" id="nav-customer" role="tabpanel" aria-labelledby="customer-tab">
                 <!-- <h4>tab 1</h4> -->
                 <!-- <img src="/C:/Sneha/projects/booking.oreva.com.au/public/img/dashboardimg.png"> -->
-                <img src="{{ asset('img/dashboardimg.png') }}" alt="dashboardimg" class="d-block mx-auto">
+                <img src="{{ asset('img/dashboardimg.png') }}" alt="dashboardimg" class="d-block mx-auto w-100">
               </div>
 
 
