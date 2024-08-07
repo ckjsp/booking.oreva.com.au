db3b20a8735b56f565d0d3b99ab905dfec96f7b0
miteshdalsaniya774 <miteshdalsaniya@jspinfotech.com>
2024-08-07 10:03:57 +0530

    change theme-default file

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,17 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-light">
-<div class="container-fluid px-0">
-
-  <p class="dashboard_text f-600 text-black"></p>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse " id="navbarNav">
-    <ul class="navbar-nav ms-auto align-items-center gap-2">
-   
-      <li class="nav-item">
-        <!-- <div class="avatar avatar-sm"><img src="{{ asset('img/Mask group (20).png') }}" alt="Avatar" class="rounded-circle pull-up" width="30px" height="30px"></div> -->
-      </li>
-    </ul>
+  <div class="container-fluid px-0">
+    <p class="dashboard_text f-600 text-black"></p>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto align-items-center gap-2">
+        <li class="nav-item">
+          <!-- <div class="avatar avatar-sm">
+            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+            </form>
+          </div> -->
+        </li>
+      </ul>
+    </div>
   </div>
-</div>
 </nav>
